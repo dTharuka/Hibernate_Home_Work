61c6d9dc1facb5b8774c51d311e5e8254e4d7cf4
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-03-23 21:02:22 +0530

    Third commit added

--- temp.js
+++ temp.js
@@ -1,3 +1,12 @@
 // initial test message
 console.log("This is my First test message on this file")
 console.log("This is my Second test message on this file")
+console.log("This is my Third test message on this file")
+
+function print () {
+  let name = "Hello World";
+}
+
+console.log("Hi there")
+
+print()
