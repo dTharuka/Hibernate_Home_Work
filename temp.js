2de3142cce1d2330192b797d8651b6e64b63cfd0
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-03-24 10:11:04 +0530

    Updated

--- temp.js
+++ temp.js
@@ -43,6 +43,18 @@ function print () {
 
 console.log("Hi there")
 
+print()
+
+console.log("This is my First test message on this file")
+console.log("This is my Second test message on this file")
+console.log("This is my Third test message on this file")
+
+function print () {
+  let name = "Hello World";
+}
+
+console.log("Hi there")
+
 console.log("Hi there")
 
 print()
